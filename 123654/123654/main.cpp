8ab48f7244134f1202aadf9da33801e351f57f92
abb <romankorchevnuy@gmail.com>
2022-08-28 12:59:11 +0300

    created project

---
+++ 123654/123654/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  123654
+//
+//  Created by abb on 28.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
